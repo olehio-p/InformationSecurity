a089a76aa2067465673a5a9105e9dcf3d0f179f6
Oleh Popovych <oleh.popovych.shi.2023@lpnu.ua>
2025-11-22 18:09:41 +0200

    Fix security issues

--- src/test/java/com/example/informationsecurity/services/CesaroTestProcessorTest.java
+++ src/test/java/com/example/informationsecurity/services/CesaroTestProcessorTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class CesaroTestProcessorTest {
+class CesaroTestProcessorTest {
 
     @InjectMocks
     private CesaroTestProcessor processor;
